384999a67b1c99b41a675330024f59158a4f2175
Luca Sbardella <luca@quantmind.com>
2017-09-30 14:52:37 +0100

    better rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
 
@@ -9,20 +8,36 @@ export default {
     output: {
         file: 'build/d3-visualize.js',
         format: 'umd',
-        sourcemap: true
+        name: 'd3',
+        sourcemap: true,
+        extend: true,
+        globals: {
+            "crossfilter": "crossfilter",
+            "d3-array": "d3",
+            "d3-canvas-transition": "d3",
+            "d3-collection": "d3",
+            "d3-dispatch": "d3",
+            "d3-dsv": "d3",
+            "d3-format": "d3",
+            "d3-let": "d3",
+            "d3-random": "d3",
+            "d3-require": "d3",
+            "d3-scale": "d3",
+            "d3-shape": "d3",
+            "d3-selection": "d3",
+            "d3-svg-legend": "d3",
+            "d3-timer": "d3",
+            "d3-time-format": "d3",
+            "d3-transition": "d3",
+            "d3-view": "d3",
+            "object-assign": "assign"
+        }
     },
-    name: 'd3',
     plugins: [
         json(),
         babel({
             babelrc: false,
             presets: ['es2015-rollup']
-        }),
-        commonjs({
-            include: [
-                // required when using yarn link
-                '../d3-view/node_modules/**'
-            ]
         }),
         sourcemaps()
     ],
@@ -45,26 +60,5 @@ export default {
         "d3-transition",
         "d3-view",
         "object-assign"
-    ],
-    globals: {
-        "crossfilter": "crossfilter",
-        "d3-array": "d3",
-        "d3-canvas-transition": "d3",
-        "d3-collection": "d3",
-        "d3-dispatch": "d3",
-        "d3-dsv": "d3",
-        "d3-format": "d3",
-        "d3-let": "d3",
-        "d3-random": "d3",
-        "d3-require": "d3",
-        "d3-scale": "d3",
-        "d3-shape": "d3",
-        "d3-selection": "d3",
-        "d3-svg-legend": "d3",
-        "d3-timer": "d3",
-        "d3-time-format": "d3",
-        "d3-transition": "d3",
-        "d3-view": "d3",
-        "object-assign": "assign"
-    }
+    ]
 };
