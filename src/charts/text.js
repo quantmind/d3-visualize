776b4e18be529283cdac2faf0883d86f0263f1f4
Luca Sbardella <luca@quantmind.com>
2017-10-15 22:51:38 +0100

    text chart

--- src/charts/text.js
+++ src/charts/text.js
@@ -1,14 +1,48 @@
 import createChart from '../core/chart';
+import textWrap from '../utils/text-wrapping';
 
 
 export default createChart('text', {
 
     options: {
-        scale: 'linear',
-        field: 'data',
-        label: 'label'
+        label: 'label',
+        data: 'data',
+        text: 'label + " " + data'
     },
 
-    doDraw () {
+    doDraw (frame) {
+        var self = this,
+            model = this.getModel(),
+            color = this.getModel('color'),
+            box = this.boundingBox(),
+            size = this.font(box),
+            paper = this.paper().size(box),
+            group = paper.group()
+                    .attr("transform", this.translate(box.total.left, box.total.top))
+                    .selectAll('text').data(frame.data),
+            width = box.innerWidth/frame.data.length,
+            widthWrap = 0.4*width,
+            store = this.dataStore;
+
+        group
+            .enter()
+                .append('text')
+                .attr("transform", shift)
+                .attr("text-anchor", "middle")
+                .attr("alignment-baseline", "middle")
+                .style("font-size", `${size}px`)
+                .style('fill-opacity', 0)
+            .merge(group)
+                .attr("transform", shift)
+                .text(d => store.eval(model.text, d))
+                .style('fill-opacity', color.fillOpacity)
+                .call(textWrap, widthWrap);
+
+        group.exit().remove();
+
+
+        function shift (d, i) {
+            return self.translate((i+0.5)*width, 0);
+        }
     }
 });
