56388638e37896dffa55699f681e99fbd05c7ee3
Luca Sbardella <luca@quantmind.com>
2017-11-08 18:31:15 +0000

    bare dependencies

--- test/karma/karma.debug.firefox.js
+++ test/karma/karma.debug.firefox.js
@@ -5,13 +5,10 @@ var base = require('./karma.base.js');
 module.exports = function (config) {
 
     var options = assign(base, {
-        browsers: ['PhantomJS', 'Chrome', 'Firefox'],
+        singleRun: false,
+        browsers: ['Firefox'],
         reporters: ['progress']
     });
-
-    if(process.env.TRAVIS || process.env.CIRCLECI) {
-        options.browsers = ['PhantomJS', 'ChromeNoSandbox', 'Firefox'];
-    }
 
     config.set(options);
 };
